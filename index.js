a014e3e24a9e5aeac50cbfd06e3947091d2a6b9f
taylorkrall2024 <156223182+taylorkrall2024@users.noreply.github.com>
2024-02-05 22:01:17 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const quotations = [
+    "Wassssuuppp! ",
+    "Good Day",
+    "How are you?",
+  ];
+  
+  const randomIndex = Math.floor(Math.random() * quotations.length);
+  const selectedQuotation = quotations[randomIndex];
+  
+  console.log(selectedQuotation);
+  
